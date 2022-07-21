666f9be3b183845b5f10a4717bdddb7e27e66ca1
Vespirit <carmrest00@gmail.com>
2022-07-21 15:23:17 -0400

    Fixed formatting

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ import re
 from gui import *
 from tkinter import filedialog
 
+
 def get_new_index(num: int, offset: int) -> str:
     new_num = str(num + offset)
-    while (len(new_num) < 3):
+    while len(new_num) < 3:
         new_num = "0" + new_num
 
     return new_num
 
+
 def ch_folder() -> str:
     return filedialog.askdirectory()
+
 
 def merge(old_folder: str, new_folder: str) -> None:
     os.chdir(old_folder)
@@ -21,7 +24,7 @@ def merge(old_folder: str, new_folder: str) -> None:
     highest_num = 0
     for file in os.listdir():
         match = ss_regex.match(file)
-        if (match):
+        if match:
             highest_num = max(int(match.group(1)), highest_num)
 
     os.chdir(new_folder)
@@ -29,11 +32,19 @@ def merge(old_folder: str, new_folder: str) -> None:
     count = 0
     for file in os.listdir():
         match = ss_regex.match(file)
-        if (match):
+        if match:
             count += 1
-            os.rename(file, old_folder + "/screenshot" + get_new_index(int(match.group(1)), highest_num) + "." + match.group(2))
+            os.rename(
+                file,
+                old_folder
+                + "/screenshot"
+                + get_new_index(int(match.group(1)), highest_num)
+                + "."
+                + match.group(2),
+            )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     initModules()
     while isRunning():
         run()
